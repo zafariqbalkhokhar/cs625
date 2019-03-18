cfd77324ba353597f6d64ac8ac6a9517bb6c97fe
zafariqbalkhokhar <47986858+zafariqbalkhokhar@users.noreply.github.com>
2019-03-18 22:35:57 +0500

    Commit of 10

---
+++ Example2.java
@@ -0,0 +1,12 @@
+public class Example2 {
+public static void main(String[] args ) {
+int var1;
+int var2;
+var1=1024;
+System.out.println("veriable one is contain="+var1);
+var2=var1/2;
+System.out.println("veriable two is contain=");
+System.out.println(var2);
+
+}
+}
---
+++ Test1.java
@@ -0,0 +1,12 @@
+public class Test1 {
+public static void main(String[] args) {
+int x=0;
+int y=0;
+while(x<5){
+x=x+1;
+y=y+x;
+}
+System.out.println(x + "" + y +" ");
+x=x+1;
+}
+}
